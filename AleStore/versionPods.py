e899cfc5a9603d39d3a423112b210451814c8331
Márcio Abrantes <abs.marcio@gmail.com>
2023-05-30 17:30:38 -0300

    improvement in the automation process

--- AleStore/versionPods.py
+++ AleStore/versionPods.py
@@ -5,6 +5,8 @@ try:
             if line == "# platform :ios, '9.0'\n":
                 line = line.replace(line, "platform :ios, '16.0'\n")
             writeList.append(line)
+            if line == "  # Pods for Infra\n":
+                writeList.append("  pod 'Alamofire', '5.7.1'\n")
     print(writeList)
     with open("Podfile", "w") as fp:
         fp.writelines(writeList)
